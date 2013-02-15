2a84b770c0afb6322b8e1a475c47a5c6dd0cff09
Pedro Pombeiro <pedropombeiro@gmail.com>
2013-02-15 23:09:24 +0100

    Add Update-AssemblyInfo-Version.ps1.

--- src/Unity.TypedFactories/Properties/AssemblyInfo.cs
+++ src/Unity.TypedFactories/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Developer In The Flow")]
 [assembly: AssemblyProduct("Unity.TypedFactories")]
-[assembly: AssemblyCopyright("Copyright © Pedro Pombeiro 2012")]
+[assembly: AssemblyCopyright("Copyright © Pedro Pombeiro 2012-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.6")]
-[assembly: AssemblyFileVersion("1.0.6")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ tools/Update-AssemblyInfo-Version.ps1
@@ -0,0 +1,8 @@
+﻿Param($AssemblyVersion, $AssemblyFileVersion, $AssemblyInformationalVersion) 
+
+$globalAssemblyInfoPath = [System.IO.Path]::GetFullPath(".\GlobalAssemblyInfo.cs")
+
+(gc $globalAssemblyInfoPath) |
+  ForEach-Object {
+  (($_ -replace "^(.*AssemblyVersion.{1}).*(.{2})$", ('$1"' + $AssemblyVersion + '"$2')) -replace "^(.*AssemblyFileVersion.{1}).*(.{2})$", ('$1"' + $AssemblyFileVersion + '"$2')) -replace "^(.*AssemblyInformationalVersion.{1}).*(.{2})$", ('$1"' + $AssemblyInformationalVersion + '"$2')
+  } | sc $globalAssemblyInfoPath
